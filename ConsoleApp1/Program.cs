074bed7134aa1c10ed82906d222ef5855c5fefd6
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-11-17 16:26:21 -0500

    Final Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class PrintExample
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Run();
+        }
+         void Run() {
+            int a = 0, b = 1, i, c, n, j;
+            Console.Write("Enter the limit: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                a = 0;
+                b = 1;
+                Console.Write(b + "\t");
+                for (j = 1; j < i; j++)
+                {
+                    c = a + b;
+                    Console.Write(c + "\t");
+                    a = b;
+                    b = c;
+                }
+                Console.Write("\n");
+            }
+
+            AnyKey();
+        }
+
+        void AnyKey()
+        {
+            Console.WriteLine("Press any key to run again..(try 369)");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ Practice/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      public class FibonacciTriangle
+        {
+            public static void Main(string[] args)
+            {
+                int n1 = 0, n2 = 1, n3, i, number;
+                Console.Write("Enter the number of elements: ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write(n1 + " " + n2 + " ");    
+                for (i = 2; i < number; ++i)                 {
+                    n3 = n1 + n2;
+                    Console.Write(n3 + " ");
+                    n1 = n2;
+                    n2 = n3;
+                }
+            }
+        }
+    }
+    }
+}
